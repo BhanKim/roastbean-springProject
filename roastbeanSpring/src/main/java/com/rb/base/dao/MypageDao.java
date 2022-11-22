ed49d5f3388b0a219b7abacac568da08274bed85
sangwonKim7 <swkim7275@gmail.com>
2022-11-22 12:19:48 +0900

    etc

--- roastbeanSpring/src/main/java/com/rb/base/dao/MypageDao.java
+++ roastbeanSpring/src/main/java/com/rb/base/dao/MypageDao.java
@@ -1,5 +1,7 @@
 package com.rb.base.dao;
 
+import java.util.List;
+
 import com.rb.base.model.MyOrderListDto;
 import com.rb.base.model.QnaDto;
 import com.rb.base.model.UserDto;
@@ -12,7 +14,7 @@ public interface MypageDao {
 	
 	public void userInfoUpdate(String user_id, String user_nick, String user_telno, String user_email, String user_addresszipcode, String user_address1, String user_address2, String user_address3) throws Exception;
 
-	public MyOrderListDto myOrderList(String user_id) throws Exception;
+	public List<MyOrderListDto> myOrderList(String user_id) throws Exception;
 	
-	public QnaDto qnaList(String user_id) throws Exception;
+	public List<QnaDto> qnaList(String user_id) throws Exception;
 }
